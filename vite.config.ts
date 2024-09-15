e630487d2a18b28ab10d634d401ebd9fa30365e7
dkulik <kulikdenis1996x@gmail.com>
2024-09-15 20:14:39 +0700

    chore: - remove template - configure primevue and inspect libs

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), Inspect()],
   base: '/interview-list/',
   server: {
     port: 3000,
