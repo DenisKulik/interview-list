0e8f2e9122e9ec998ccbf2f5c46d7fd86dea4bd3
dkulik <kulikdenis1996x@gmail.com>
2024-09-23 20:12:03 +0700

    feat: - add app header - configure primeflex to the project

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { AppHeader } from '@/components'
+</script>
 
 <template>
-  <RouterView />
+  <div class="container">
+    <app-header />
+    <div class="content">
+      <RouterView />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
-import 'primeicons/primeicons.css'
+import Menubar from 'primevue/menubar'
 
 import App from './App.vue'
 import router from './router'
+
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.component('app-menubar', Menubar)
 app.use(PrimeVue, {
   theme: {
     preset: Aura
